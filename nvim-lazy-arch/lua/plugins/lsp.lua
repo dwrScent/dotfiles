f225f543a13a197a206718ee15d41400b82ea198
Ubuntu <ubuntu@ip-172-26-10-126.ap-northeast-1.compute.internal>
2026-08-09 01:35:07 +0000

    cancel Mason auto update and install

--- nvim-lazy-arch/lua/plugins/lsp.lua
+++ nvim-lazy-arch/lua/plugins/lsp.lua
@@ -1,28 +1,17 @@
 return {
     {
         "williamboman/mason.nvim",
-        build = ":MasonUpdate",
         dependencies = {
             "williamboman/mason-lspconfig.nvim",
             "neovim/nvim-lspconfig",
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
         },
         config = function()
             require("mason").setup()
 
             require("mason-lspconfig").setup({
-                ensure_installed = { "pyright", "clangd" },
+                -- Use :Mason to install language servers manually.
+                ensure_installed = {},
                 automatic_enable = true,
-            })
-
-            require("mason-tool-installer").setup({
-                ensure_installed = {
-                    "stylua",
-                    "black",
-                    "flake8",
-                    "clang-format",
-                },
-                run_on_start = true,
             })
         end,
     },
